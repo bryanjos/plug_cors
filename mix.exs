b3f1fe8a0e05190985ee25ffcbd6b179ee673f15
Bryan Joseph <bryanjos@gmail.com>
2014-10-04 07:57:34 -0500

    Updated version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PlugCors.Mixfile do
 
   def project do
     [ app: :plug_cors,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.0.0",
       description: description,
       package: package,
